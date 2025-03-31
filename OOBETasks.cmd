650aa5a498e7a31ee1928dbf0af3200019ee9b4d
pbuchinger <137380664+pbuchinger@users.noreply.github.com>
2025-03-31 15:53:36 +0200

    Create OOBETasks.cmd

---
+++ OOBETasks.cmd
@@ -0,0 +1,11 @@
+Write-Host -ForegroundColor Green "Create C:\Windows\System32\OOBETasks.CMD"
+$OOBETasksCMD = @'
+PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
+Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
+Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
+Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
+Start /Wait PowerShell -NoL -C Start-AutopilotOOBE
+Start /Wait PowerShell -NoL -C Start-OOBEDeploy
+Start /Wait PowerShell -NoL -C Restart-Computer -Force
+'@
+$OOBETasksCMD | Out-File -FilePath 'C:\Windows\System32\OOBETasks.CMD' -Encoding ascii -Force
